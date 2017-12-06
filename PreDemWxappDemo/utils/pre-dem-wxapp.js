efc0f0c93d7eed33949a6a94d1ff27fd9da77f0f
Siyu Wang <cn.wangsiyu@gmail.com>
2017-12-06 22:00:03 +0800

    update

--- PreDemWxappDemo/utils/pre-dem-wxapp.js
+++ PreDemWxappDemo/utils/pre-dem-wxapp.js
@@ -1,4 +1,14 @@
-/******/ (function(modules) { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["predem"] = factory();
+	else
+		root["predem"] = factory();
+})(typeof self !== 'undefined' ? self : this, function() {
+return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -345,3 +355,4 @@ module.exports = {
 
 /***/ })
 /******/ ]);
+});
