4e58011e2466fc78b7feef5e9824c440f4385af9
Siyu Wang <cn.wangsiyu@gmail.com>
2017-12-06 11:45:47 +0800

    update

--- utils/pre-dem-wxapp.js
+++ utils/pre-dem-wxapp.js
@@ -6,17 +6,23 @@ var _openId = '', _domain = '', _appId = '', _appVersion = ''
 
 const generateMetadata = () => {
   const systemInfo = wx.getSystemInfoSync()
-  return {
+  const pages = getCurrentPages()
+  var ret = {
     time: Date.now(),
-    app_version: _appVersion,
     sdk_version: SdkVersion,
     sdk_id: generateUuid(),
-    tag: _openId,
     device_model: systemInfo.model,
     mini_program_type: 'WeChat',
     mini_program_version: systemInfo.version,
     mini_program_sdk_version: systemInfo.SDKVersion
   }
+  _appVersion && (ret.app_version = _appVersion)
+  _openId && (ret.tag = _openId)
+  if (pages.length != 0) {
+    let route = pages[pages.length - 1].route
+    route && (ret.path = route)
+  }
+  return ret
 }
 
 const sendEvent = (subPath, event) => {
@@ -54,14 +60,85 @@ const init = (domain, appKey, appVersion, openId) => {
   _openId = openId || ''
 }
 
+const request = (requestObject) => {
+  var content = {
+    start_timestamp: Date.now(),
+    method: requestObject.method,
+  }
+  let domainAndPath = parseUrl(requestObject.url)
+  domainAndPath.domain && (content.domain = domainAndPath.domain)
+  domainAndPath.path && (content.path = domainAndPath.path)
+
+  var newRequestObject = Object.assign({}, requestObject)
+  newRequestObject.success = (ret) => {
+    content.end_timestamp = Date.now()
+    content.status_code = ret.statusCode
+    content.data_length = JSON.stringify(ret.data).length
+    requestObject.success(ret)
+    sendCustomEvent(content)
+  }
+  newRequestObject.fail = (ret) => {
+    content.end_timestamp = Date.now()
+    content.network_error_msg = ret.errMsg
+    requestObject.fail(ret)
+    sendCustomEvent(content)
+  }
+  wx.request(newRequestObject)
+}
+
+const parseUrl = (url) => {
+  var domain = "";
+  var path = "";
+  const array1 = url.split("//");
+  var hostAndPath
+  if (array1.length === 1) {
+    hostAndPath = array1[0]
+  } else {
+    hostAndPath = array1[1]
+  }
+  let slashIndex = hostAndPath.indexOf('/')
+  if (slashIndex == -1) {
+    domain = hostAndPath
+  } else {
+    domain = hostAndPath.substring(0, slashIndex)
+    path = hostAndPath.substring(slashIndex, hostAndPath.length)
+  }
+  return { domain, path }
+}
+
+const debug = console.debug
+console.debug = (obj) => {
+  debug(obj)
+}
+
+const info = console.info
+console.info = (obj) => {
+  info(obj)
+}
+
+const warn = console.warn
+console.warn = (obj) => {
+  warn(obj)
+}
+
+const error = console.error
+console.error = (obj) => {
+  error(obj)
+}
+
 module.exports = {
   init,
   sendCustomEvent
 }
 
-setTimeout(()=> {
+setTimeout(() => {
   console.info(generateMetadata())
   console.info(wx.getSystemInfoSync())
-  // sendEvent('app-config', generateMetadata())
-}, 100)
+  request({
+    url: 'http://www.baidu.com',
+    success: (ret) => {
+      console.log(ret)
+    }
+  })
+}, 1000)
 
