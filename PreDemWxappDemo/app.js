d8a56f6c1335d08911f09d641cb4b3d415c97b02
Siyu Wang <cn.wangsiyu@gmail.com>
2017-12-14 17:35:25 +0800

    update OpenId get method

--- PreDemWxappDemo/app.js
+++ PreDemWxappDemo/app.js
@@ -3,19 +3,12 @@ const dem = require('./utils/pre-dem-wxapp.js')
 
 App({
   onLaunch: () => {
-    console.log('on lauch')
     wx.login({
       success: res => {
         if (res.code) {
-          let data = {
-            appid: '此处填写您的在微信小程序后台获取到的 app id',
-            secret: '此处填写您的在微信小程序后台获取到的 app secret',
-            js_code: res.code,
-            grant_type: 'authorization_code'
-          }
           wx.request({
-            url: 'https://api.weixin.qq.com/sns/jscode2session',
-            data,
+            url: '此处填写您的获取OpenId的服务地址',
+            data: res.code,
             success: res => {
               res.openid && dem.setOpenId(res.openid)
             }
